f0734a2735bfd3a4db63fc4f20f5f9cb0e8cf2a5
gitlalihi <t6916417@gmail.com>
2023-10-25 15:48:26 +0530

    Changed variable and value of ascii

--- alphabetpyramid.py
+++ alphabetpyramid.py
@@ -1,10 +1,10 @@
-num= int(input( " Enter your  pattern number"))
+x= int(input( " Enter your  pattern number"))
 
-def alphabet(num):
-    asciiValue = 65
-    for i in range(asciiValue, asciiValue+num+1):
+def alphabet(x):
+    asciiValue = 70
+    for i in range(asciiValue, asciiValue+x+1):
         for j in range(i, 64, -1):
             print(chr(j), end="")
         print()
             
-alphabet(num)
+alphabet(x)
